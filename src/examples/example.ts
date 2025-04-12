435abfb8d20a99d8855a29fb950dfa174b056054
Caius Citiriga <cc@kiboko.it>
2025-04-12 04:35:51 +0200

    fix: preserving buffer state between chunks to avoid losing large messages

--- src/examples/example.ts
+++ src/examples/example.ts
@@ -1,25 +1,28 @@
 import { resolve } from 'path';
 import { Synapse } from '../index';
 import { tap } from 'rxjs';
+import { HimalayaHistoryLoader } from '@himalaya-quant/history-loader';
+import { randomUUID } from 'crypto';
+
 const synapse = new Synapse();
 
 async function main() {
-    synapse.dispose();
-    await synapse.spawn(resolve(__dirname, '../../py_test'), 'main.py');
-    synapse.instanceLogs.pipe(tap((log) => console.log(log))).subscribe();
+  await synapse.spawn(resolve(__dirname, '../../py_test'), 'main.py');
+  synapse.instanceLogs.pipe(tap((log) => console.log(log))).subscribe();
 
-    let finalMessage = '';
-    for (const msg of [
-        { text: 'This is an example payload' },
-        { cmd: 'run_function_x', params: { foo: 'bar', baz: 10 } },
-        [[Date.now(), 12.5, 12.9, 12.3, 12.6, 345]],
-    ]) {
-        const result = await synapse.call(msg);
-        finalMessage += `${result.data}\n`;
-    }
+  const historicData = await HimalayaHistoryLoader.loadHistory({
+    timeFrame: '1h',
+    symbol: 'BTC/USDT',
+    exchangeMarket: 'binance_spot',
+    // toDate: '2025-01-02T00:00:00Z',
+    toDate: new Date(),
+    requestId: randomUUID(),
+    fromDate: new Date(Date.parse('2020-01-01T00:00:00Z')),
+  });
 
-    await synapse.dispose();
-    console.log(finalMessage);
+  const result = await synapse.call(historicData.history);
+  await synapse.dispose();
+  console.log(result);
 }
 
 main().then();
--- src/modules/instance-manager.service.ts
+++ src/modules/instance-manager.service.ts
@@ -4,267 +4,261 @@ import { Observable, Subject, Subscription, tap } from 'rxjs';
 import { encode, decode } from '@msgpack/msgpack';
 
 import {
-    spawn,
-    spawnSync,
-    ChildProcessWithoutNullStreams,
+  spawn,
+  spawnSync,
+  ChildProcessWithoutNullStreams,
 } from 'child_process';
 
 type QueuedInput = {
-    input: any;
-    resolver: (value: any) => void;
+  input: any;
+  resolver: (value: any) => void;
 };
 
 export class InstanceManger {
-    private readonly inputsQueue: QueuedInput[] = [];
-    private readonly instanceOutputs$ = new Subject<any>();
-    private readonly instanceLogs$ = new Subject<string>();
-    private readonly instanceInputs$ = new Subject<QueuedInput>();
+  private readonly inputsQueue: QueuedInput[] = [];
+  private readonly instanceOutputs$ = new Subject<any>();
+  private readonly instanceLogs$ = new Subject<string>();
+  private readonly instanceInputs$ = new Subject<QueuedInput>();
 
-    private instance!: ChildProcessWithoutNullStreams;
-    private currentInputResolver!: (value: any) => void;
-    private instanceInputStreamSubscription!: Subscription;
-    private instanceOutputStreamSubscription!: Subscription;
+  private messageBuffer = Buffer.alloc(0);
+  private instance!: ChildProcessWithoutNullStreams;
+  private currentInputResolver!: (value: any) => void;
+  private instanceInputStreamSubscription!: Subscription;
+  private instanceOutputStreamSubscription!: Subscription;
 
-    /**
-     * Subscription to the instance logs.
-     * If you want your script logs to be seen by Synapse, in your python script
-     * always print to the stderr like:
-     *
-     * ```py
-     * import sys
-     *
-     * print("my log", file=sys.stderr)
-     * ```
-     *
-     * every log written this way will pass through this stream.
-     *
-     * @returns {Observable} The observable to which you can subscribe to access
-     * the logs stream.
-     */
-    get instanceLogs() {
-        return this.instanceLogs$.asObservable();
+  /**
+   * Subscription to the instance logs.
+   * If you want your script logs to be seen by Synapse, in your python script
+   * always print to the stderr like:
+   *
+   * ```py
+   * import sys
+   *
+   * print("my log", file=sys.stderr)
+   * ```
+   *
+   * every log written this way will pass through this stream.
+   *
+   * @returns {Observable} The observable to which you can subscribe to access
+   * the logs stream.
+   */
+  get instanceLogs() {
+    return this.instanceLogs$.asObservable();
+  }
+
+  /**
+   * Calls the spawned python instance with the given input.
+   * Throws if the instance has not been spawned first.
+   *
+   * @param {object} input Any simple JSON structure will be accepted.
+   * For more details see: https://msgpack.org/
+   *
+   * @returns {ResultType} The result returned from your python script.
+   * @throws {Error} If the instance has not been spawned.
+   */
+  call<ResultType = any>(input: any): Promise<ResultType> {
+    if (!this.instance)
+      throw new Error(`Cannot send inputs to instance before spawning it.`);
+
+    return new Promise((resolver) => {
+      this.inputsQueue.push({ input, resolver });
+      if (this.inputsQueue.length === 1)
+        this.instanceInputs$.next(this.inputsQueue[0]);
+    });
+  }
+
+  /**
+   * Spawns a new python script instance and keeps it alive until dispose is
+   * called. After the spawning you can safely start sending messages to it.
+   * Throws if there's an error during the spawning process.
+   *
+   * What it does:
+   * - Postfixes the extension .py to the entrypoint if missing
+   * - Ensures that the paths are correct and the requirements.txt exists
+   * - Creates a dedicated Python virtual environment
+   * - Installs dependencies via requirements.txt
+   * - Spawns Python script as subprocess
+   * - Reuses instance until explicit disposal avoiding spawn overhead
+   *
+   * @param {string} directory The path pointing to the python module directory.
+   * @param {string} entrypoint The entrypoint file name.
+   *
+   * @returns {Promise<void>} A promise that resolves once the spawn completes.
+   * @throws {Error} If there's an error during the spawning process.
+   */
+  async spawn(directory: string, entrypoint: string): Promise<void> {
+    entrypoint = this.postfixExtension(entrypoint);
+
+    this.ensureExistsOrThrow(directory, entrypoint);
+
+    if (!existsSync(join(directory, '.venv'))) {
+      this.createVirtualEnv(directory);
+      await this.installDependencies(directory);
     }
 
-    /**
-     * Calls the spawned python instance with the given input.
-     * Throws if the instance has not been spawned first.
-     *
-     * @param {object} input Any simple JSON structure will be accepted.
-     * For more details see: https://msgpack.org/
-     *
-     * @returns {ResultType} The result returned from your python script.
-     * @throws {Error} If the instance has not been spawned.
-     */
-    call<ResultType = any>(input: any): Promise<ResultType> {
-        if (!this.instance)
-            throw new Error(
-                `Cannot send inputs to instance before spawning it.`
-            );
+    await this.spawnInstance(directory, entrypoint);
+    this.openSubscriptions();
+  }
 
-        return new Promise((resolver) => {
-            this.inputsQueue.push({ input, resolver });
-            if (this.inputsQueue.length === 1)
-                this.instanceInputs$.next(this.inputsQueue[0]);
-        });
+  /**
+   * Disposes the instance, closing the stdin stream, all the subscriptions
+   * and tries to kill the instance.
+   * Manages graceful and forceful termination, first tries with a SIGTERM, if
+   * after 500ms is not killed, will force a SIGKILL.
+   * Throws if after the SIGKILL the instance is still alive.
+   *
+   * After dispose has been called, you will have to call spawn again. Trying
+   * to send any messages after dispose, will result in an error.
+   *
+   * @returns {Promise<void>} Resolves once the dispose has been done.
+   * @throws {Error} If after forceful termination the instance is still alive
+   */
+  async dispose() {
+    this.instanceInputStreamSubscription?.unsubscribe();
+    this.instanceOutputStreamSubscription?.unsubscribe();
+
+    if (!this.instance) return;
+
+    this.instance.stdin.end();
+    this.instance.kill('SIGTERM');
+    const isTerminated = await this.waitForTermination(500);
+
+    if (!isTerminated) {
+      console.log(
+        'Instance did not close gracefully in 500ms. Forcing SIGKILL.'
+      );
+      this.instance.kill('SIGKILL');
+      await this.waitForTermination(500);
     }
 
-    /**
-     * Spawns a new python script instance and keeps it alive until dispose is
-     * called. After the spawning you can safely start sending messages to it.
-     * Throws if there's an error during the spawning process.
-     *
-     * What it does:
-     * - Postfixes the extension .py to the entrypoint if missing
-     * - Ensures that the paths are correct and the requirements.txt exists
-     * - Creates a dedicated Python virtual environment
-     * - Installs dependencies via requirements.txt
-     * - Spawns Python script as subprocess
-     * - Reuses instance until explicit disposal avoiding spawn overhead
-     *
-     * @param {string} directory The path pointing to the python module directory.
-     * @param {string} entrypoint The entrypoint file name.
-     *
-     * @returns {Promise<void>} A promise that resolves once the spawn completes.
-     * @throws {Error} If there's an error during the spawning process.
-     */
-    async spawn(directory: string, entrypoint: string): Promise<void> {
-        entrypoint = this.postfixExtension(entrypoint);
+    if (!this.instance.killed)
+      throw new Error(`Cannot kill instance with PID: ${this.instance.pid}`);
+  }
 
-        this.ensureExistsOrThrow(directory, entrypoint);
+  private waitForTermination(timeout: number): Promise<boolean> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.instance.killed);
+      }, timeout);
+    });
+  }
 
-        if (!existsSync(join(directory, '.venv'))) {
-            this.createVirtualEnv(directory);
-            await this.installDependencies(directory);
+  private ensureExistsOrThrow(directory: string, entrypoint: string) {
+    if (!existsSync(directory))
+      throw new Error(`Directory "${directory}" does not exist.`);
+    if (!existsSync(join(directory, entrypoint)))
+      throw new Error(`Entrypoint "${entrypoint}" does not exist.`);
+    if (!existsSync(join(directory, 'requirements.txt')))
+      throw new Error(`"requirements.txt" file is missing.`);
+  }
+
+  private createVirtualEnv(directory: string) {
+    spawnSync('python', ['-m', 'venv', join(directory, '.venv')]);
+  }
+
+  private installDependencies(directory: string) {
+    const python = this.getVirtualEnvPythonInterpreter(directory);
+    const requirementsPath = join(directory, 'requirements.txt');
+    const installDeps = spawn(python, [
+      '-m',
+      'pip',
+      'install',
+      '-r',
+      requirementsPath,
+    ]);
+
+    return new Promise<void>((resolve) => {
+      installDeps.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+
+      installDeps.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+      });
+
+      installDeps.on('close', async (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          throw new Error(`Dependencies install failure: ${code}`);
         }
+      });
+    });
+  }
 
-        await this.spawnInstance(directory, entrypoint);
-        this.openSubscriptions();
+  private getVirtualEnvPythonInterpreter(directory: string) {
+    return join(directory, '.venv', 'bin', 'python');
+  }
+
+  private async spawnInstance(directory: string, entrypoint: string) {
+    this.instance = spawn(this.getVirtualEnvPythonInterpreter(directory), [
+      join(directory, entrypoint),
+    ]);
+
+    this.instance.stdout.on('data', (chunk) => {
+      this.handleChunk(chunk);
+    });
+
+    // we expect all messages (logs, errors, etc) that is not the actual
+    // response on the stderr stream so we don't touch the stdin encoding
+    this.instance.stderr.on('data', (chunk) => {
+      const msg = `🐍 Python: ${chunk.toString()}`;
+      this.instanceLogs$.next(msg);
+    });
+  }
+
+  private packAndSend(message: any) {
+    const payload = encode(message);
+    const lengthBuffer = Buffer.alloc(4); // 4 bytes for the payload size
+    lengthBuffer.writeUint32LE(payload.length, 0);
+
+    this.instance.stdin.write(lengthBuffer);
+    this.instance.stdin.write(payload);
+  }
+
+  private postfixExtension(entrypoint: string) {
+    return entrypoint.endsWith('.py') ? entrypoint : `${entrypoint}.py`;
+  }
+
+  private handleChunk = (chunk: Buffer) => {
+    this.messageBuffer = Buffer.concat([this.messageBuffer, chunk]);
+
+    while (this.messageBuffer.length >= 4) {
+      const messageLength = this.messageBuffer.readUInt32LE(0);
+
+      if (this.messageBuffer.length >= 4 + messageLength) {
+        const messagePayload = this.messageBuffer.slice(4, 4 + messageLength);
+        const decoded = decode(messagePayload);
+        this.instanceOutputs$.next(decoded);
+
+        this.messageBuffer = this.messageBuffer.slice(4 + messageLength);
+      } else {
+        // Not enough data yet
+        break;
+      }
     }
+  };
 
-    /**
-     * Disposes the instance, closing the stdin stream, all the subscriptions
-     * and tries to kill the instance.
-     * Manages graceful and forceful termination, first tries with a SIGTERM, if
-     * after 500ms is not killed, will force a SIGKILL.
-     * Throws if after the SIGKILL the instance is still alive.
-     *
-     * After dispose has been called, you will have to call spawn again. Trying
-     * to send any messages after dispose, will result in an error.
-     *
-     * @returns {Promise<void>} Resolves once the dispose has been done.
-     * @throws {Error} If after forceful termination the instance is still alive
-     */
-    async dispose() {
-        this.instanceInputStreamSubscription?.unsubscribe();
-        this.instanceOutputStreamSubscription?.unsubscribe();
+  private openSubscriptions() {
+    this.instanceInputStreamSubscription = this.instanceInputs$
+      .pipe(
+        tap(({ input, resolver }) => {
+          this.currentInputResolver = resolver;
+          this.packAndSend(input);
+        })
+      )
+      .subscribe();
 
-        if (!this.instance) return;
-
-        this.instance.stdin.end();
-        this.instance.kill('SIGTERM');
-        const isTerminated = await this.waitForTermination(500);
-
-        if (!isTerminated) {
-            console.log(
-                'Instance did not close gracefully in 500ms. Forcing SIGKILL.'
-            );
-            this.instance.kill('SIGKILL');
-            await this.waitForTermination(500);
-        }
-
-        if (!this.instance.killed)
-            throw new Error(
-                `Cannot kill instance with PID: ${this.instance.pid}`
-            );
-    }
-
-    private waitForTermination(timeout: number): Promise<boolean> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(this.instance.killed);
-            }, timeout);
-        });
-    }
-
-    private ensureExistsOrThrow(directory: string, entrypoint: string) {
-        if (!existsSync(directory))
-            throw new Error(`Directory "${directory}" does not exist.`);
-        if (!existsSync(join(directory, entrypoint)))
-            throw new Error(`Entrypoint "${entrypoint}" does not exist.`);
-        if (!existsSync(join(directory, 'requirements.txt')))
-            throw new Error(`"requirements.txt" file is missing.`);
-    }
-
-    private createVirtualEnv(directory: string) {
-        spawnSync('python', ['-m', 'venv', join(directory, '.venv')]);
-    }
-
-    private installDependencies(directory: string) {
-        const python = this.getVirtualEnvPythonInterpreter(directory);
-        const requirementsPath = join(directory, 'requirements.txt');
-        const installDeps = spawn(python, [
-            '-m',
-            'pip',
-            'install',
-            '-r',
-            requirementsPath,
-        ]);
-
-        return new Promise<void>((resolve) => {
-            installDeps.stdout.on('data', (data) => {
-                console.log(`stdout: ${data}`);
-            });
-
-            installDeps.stderr.on('data', (data) => {
-                console.log(`stderr: ${data}`);
-            });
-
-            installDeps.on('close', async (code) => {
-                if (code === 0) {
-                    resolve();
-                } else {
-                    throw new Error(`Dependencies install failure: ${code}`);
-                }
-            });
-        });
-    }
-
-    private getVirtualEnvPythonInterpreter(directory: string) {
-        return join(directory, '.venv', 'bin', 'python');
-    }
-
-    private async spawnInstance(directory: string, entrypoint: string) {
-        this.instance = spawn(this.getVirtualEnvPythonInterpreter(directory), [
-            join(directory, entrypoint),
-        ]);
-
-        this.instance.stdout.on('data', (chunk) => {
-            this.handleChunk(chunk);
-        });
-
-        // we expect all messages (logs, errors, etc) that is not the actual
-        // response on the stderr stream so we don't touch the stdin encoding
-        this.instance.stderr.on('data', (chunk) => {
-            const msg = `🐍 Python: ${chunk.toString()}`;
-            this.instanceLogs$.next(msg);
-        });
-    }
-
-    private packAndSend(message: any) {
-        const payload = encode(message);
-        const lengthBuffer = Buffer.alloc(4); // 4 bytes for the payload size
-        lengthBuffer.writeUint32LE(payload.length, 0);
-
-        this.instance.stdin.write(lengthBuffer);
-        this.instance.stdin.write(payload);
-    }
-
-    private postfixExtension(entrypoint: string) {
-        return entrypoint.endsWith('.py') ? entrypoint : `${entrypoint}.py`;
-    }
-
-    private handleChunk = (chunk: Buffer, messageBuffer = Buffer.alloc(0)) => {
-        messageBuffer = Buffer.concat([messageBuffer, chunk]);
-
-        while (messageBuffer.length >= 4) {
-            const messageLength = messageBuffer.readUInt32LE(0);
-
-            if (messageBuffer.length >= 4 + messageLength) {
-                const messagePayload = messageBuffer.slice(
-                    4,
-                    4 + messageLength
-                );
-                const decoded = decode(messagePayload);
-                this.instanceOutputs$.next(decoded);
-
-                messageBuffer = messageBuffer.slice(4 + messageLength);
-            } else {
-                // Not enough data yet
-                break;
-            }
-        }
-    };
-
-    private openSubscriptions() {
-        this.instanceInputStreamSubscription = this.instanceInputs$
-            .pipe(
-                tap(({ input, resolver }) => {
-                    this.currentInputResolver = resolver;
-                    this.packAndSend(input);
-                })
-            )
-            .subscribe();
-
-        this.instanceOutputStreamSubscription = this.instanceOutputs$
-            .pipe(
-                tap((output) => {
-                    this.currentInputResolver(output);
-                    // job done, no need for it anymore
-                    this.inputsQueue.shift();
-                    if (this.inputsQueue.length)
-                        this.instanceInputs$.next(this.inputsQueue[0]);
-                })
-            )
-            .subscribe();
-    }
+    this.instanceOutputStreamSubscription = this.instanceOutputs$
+      .pipe(
+        tap((output) => {
+          this.currentInputResolver(output);
+          // job done, no need for it anymore
+          this.inputsQueue.shift();
+          if (this.inputsQueue.length)
+            this.instanceInputs$.next(this.inputsQueue[0]);
+        })
+      )
+      .subscribe();
+  }
 }
